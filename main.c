951a655d10c955aea1b91dd3494f787a09ab5e1d
MaxJ02 <114068285+MaxJ02@users.noreply.github.com>
2022-12-29 13:51:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include "car.h"
 
+/********************************************************************************
+* Detta är versionen av main för G uppgiften. Versionen med Vtables finns bifogad
+* som main2.c
+********************************************************************************/
 
+/**************************************************************************************************************************
+* main: Program som skapar 3 olika car objekt genom strukten car. Tilldelar attribut till dessa
+* och placerar de sedan i strukten cars. Sedan skrivs varje bil ut för sig via anropp av funktionen
+* car_print till angiven utström, i detta fall stdout. Bil 3 ändrar sedan färg och växellåda
+* genom anropp av funktionerna car_change_color och car_change_transmission. Bil 3 skrivs sedan
+* ut igen för att kontrollera att ändringarna verkligen sker. Sedan Öppnar och skriver till filen "cars.txt",
+* om filpekaren returnerar null skrivs ett felutmeddelande ut och 1 returneras för att indikera att ett fel har skett.
+* Lyckas däremot programmet öppna och skriva i filen "cars.txt så skrivs innehållet ur cars arrayen ut till filen via funktionen car_print.
+* Filen cars.txt stängs och sedan skriver programmet ut innehållet från angiven fil rad för rad, i angiven utström.
+*******************************************************************************************************************************/
 
 
 
@@ -21,9 +35,12 @@ int main(void)
 	car_change_color(&car3, "Green");
 
 	car_change_transmission(&car3);
-
+        
+	//Skriver ut car3 igen för att kontrollera att ändringarna lyckades.
 	car_print(&car3, stdout);
 
+	
+	//Öppnar och skriver till filen "cars.txt", om filpekaren returnerar null skrivs ett felutmeddelande ut och 1 returneras för att indikera att ett fel har skett.
 	FILE* fp = fopen("cars.txt", "w");
 	if (fp == NULL) {
 		perror("Error opening file");
@@ -39,7 +56,7 @@ int main(void)
 	// Stänger filen cars.txt
 	fclose(fp);
 
-	//Skriver ut innehållet i angiven fil rad för rad, i angiven utström. 
+	//Skriver ut innehållet från angiven fil rad för rad, i angiven utström.
 	file_read("cars.txt", stdout);
 
 	return 0;
